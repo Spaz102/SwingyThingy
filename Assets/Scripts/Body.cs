aff3a54059a65e026167bea4b09293fd66266249
Christian <Spaz.rabbit.102@gmail.com>
2020-01-26 09:59:41 -0500

    Adding unity project. Hello world!

---
+++ Assets/Scripts/Body.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body: MonoBehaviour {
+	public GameObject controller;
+	public float force = 10f;
+	public float mass = 10f;
+
+	void Update() {
+		Vector3 toTarget = controller.transform.position - this.transform.position;
+		toTarget = toTarget.normalized * Mathf.Min(toTarget.magnitude, force);
+		
+		this.transform.position = this.transform.position + toTarget;
+	}
+}
---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor: MonoBehaviour {
+	void Start() {
+
+	}
+
+	void Update() {
+		this.transform.position = Input.mousePosition;
+	}
+}
